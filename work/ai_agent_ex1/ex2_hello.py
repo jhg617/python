3952ed7fc340f594bcdd5f1b1d1e22d592ced3f0
jhg617 <gksruf2233@gmail.com>
2025-10-24 11:01:45 +0900

    Add multi-turn conversation support and second API call

---
+++ work/ai_agent_ex1/ex2_hello.py
@@ -0,0 +1,38 @@
+"""
+먼저 Anthropic 패키지를 설치하자!
+pip install anthropic==0.49.0
+pip install python-dotenv
+"""
+import anthropic
+from dotenv import load_dotenv
+import os
+
+# .env파일 로드
+load_dotenv()
+
+# 원하는 ANTHROPIC_API_KEY값 가져오기
+api_key = os.getenv("ANTHROPIC_API_KEY")
+client = anthropic.Anthropic(api_key=api_key)
+
+conversation = [] # 대화기록을 저장할 곳
+
+# 사용자의 입력값(프롬프트) 추가
+conversation.append({"role":"user","content":"안녕! 나는 hgjeon이야!"})
+
+response = client.messages.create(
+    model="claude-3-5-haiku-latest",
+    max_tokens=1000,
+    messages=conversation
+)
+assistant_message = response.content[0].text
+print(assistant_message)
+
+# 사용자 입력값 추가
+conversation.append({"role":"user","content":"내가 누구라고 했지?"})
+
+response = client.messages.create(
+    model="claude-3-5-haiku-latest",
+    max_tokens=1000,
+    messages=conversation
+)
+print(response.content[0].text)
